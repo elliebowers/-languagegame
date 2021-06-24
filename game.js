9f02f479c41a01deddaf4ada9a17a85299eeb076
polly0619 <78048114+polly0619@users.noreply.github.com>
2021-06-24 21:58:38 +0900

    changes

---
+++ game.js
@@ -0,0 +1,43 @@
+/*
+const YOUTUBE_API_KEY = 'AIzaSyCDB3M78P11OokQ01YEaxED6xq3RQ_M0lE';
+const searchTerms = ["urdu%20song","korean%20song", "russian%20song", "georgian%20song", "spanish%20song", "arabic%20song"];
+
+const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
+
+const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${YOUTUBE_API_KEY}`;
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    document.querySelector(".youtubeVideo").src = `https://www.youtube.com/embed/${data.items[0].id.videoId}`;
+});
+*/
+
+let start = document.querySelector("#start");
+start.addEventListener('click', function(event){
+    console.log("clicked");
+ event.preventDefault();
+
+let section = document.createElement("section");
+let iframe = document.createElement("iframe");
+iframe.width = "500px";
+iframe.height = "300px";
+iframe.className= "video";
+section.appendChild(iframe);
+document.body.appendChild(section);
+let divBtn = document.createElement("div");
+divBtn.className = "btn-style";
+document.body.appendChild(divBtn);
+let array = ["Urdu","Korean","Georgian","Spanish","Arabic","Russian"]
+for(let i =0;i<6;i++){
+let value = array[i];
+let btn = document.createElement("button");
+//btn.innerHTML = "Submit";
+btn.innerText = value;
+btn.type = "submit";
+btn.name = "btn"+i;
+btn.className="btn"+i;
+console.log(btn.name);
+divBtn.appendChild(btn);
+}
+
+});
